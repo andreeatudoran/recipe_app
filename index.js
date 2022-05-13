73a8d96d89a19afbb9548a2119c6368b91491ef7
Andreea <tudoranandreea18@gmail.com>
2022-05-14 02:35:30 +0300

    Update

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express')
-const cors = require('cors')
+const messagesRouter = require("./router/messagesRouter");
+const bodyParser = require("body-parser");
+
 const app = express();
-app.use(cors())
+
+// for parsing application/json
+app.use(bodyParser.json()); 
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/messages", messagesRouter);
 
 //am creat un server aici
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
 app.listen(port, () => {
   console.log(`Recipe app listening on port ${port}!`)
 });
+
